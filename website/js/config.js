c02f5017cc62bc164aede5b7a00231bac7eba560
jqu <jn679016@dal.ca>
2022-03-07 15:20:36 -0400

    finish login function

--- website/js/config.js
+++ website/js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_aL0DC6l45', // e.g. us-east-2_uXboG5pAb
+        userPoolId: 'us-east-1_SWRPY4xEg', // e.g. us-east-2_uXboG5pAb
         region: 'us-east-1', // e.g. us-east-2
-        clientId: '2dfpd48172t7791f5eq53frjfk' //is this used anywhere?
+        clientId: '6kmcvdh8uucnn6tiluelpvde7a' //is this used anywhere?
     },
 };
