a6e76c7d0a727e1cdf1ac956af84099cc6202625
jqu <jn679016@dal.ca>
2022-03-07 18:48:08 -0400

    fix bug in login.html

--- website/js/loginscript.js
+++ website/js/loginscript.js
@@ -1,117 +1,144 @@
-
-
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
 
 
-
 signUpButton.addEventListener('click', () => {
 
-	container.classList.add("right-panel-active");
+    container.classList.add("right-panel-active");
 });
 
 signInButton.addEventListener('click', () => {
-	container.classList.remove("right-panel-active");
+    container.classList.remove("right-panel-active");
 });
 
 
+var username;
+var password;
+var email;
+var poolData;
 
-	var username;
-	var password;
-	var email;
-	var poolData;
+//intercept submit process, use AWS Cognito Script to control
+function processLogin(e) {
+    if (e.preventDefault) e.preventDefault();
 
-	function cleanPage(){
-	document.getElementById("loginPassword").value="";
-	document.getElementById("loginUsername").value="";
-	document.getElementById("response").innerText="";
+    login();
+    return false;
+}
+
+function processRegister(e) {
+    if (e.preventDefault) e.preventDefault();
+
+    register();
+    return false;
 }
 
 
-	function register(){
-
-
-	username=document.getElementById("regUserName").value;
-	password=document.getElementById("regPassword").value;
-	email=document.getElementById("regEmail").value;
-
-	var pattern = /[\da-zA-Z]+/;
-	var digit=/[\d]+/;
-	var letter=/[a-zA-Z]+/;
-	var emailRegex=/[^@\s]+@[^@\s]+\.[^@\s]+/;
-	if (pattern.test(username)&&pattern.test(password)&&password.length>=8&&digit.test(password)&&letter.test(password)&&emailRegex.test(email)){
-
-	poolData={
-	UserPoolId: _config.cognito.userPoolId,//AWS Cognito Userpool ID
-	ClientId: _config.cognito.clientId,
-};
-	var userPool=new AmazonCognitoIdentity.CognitoUserPool(poolData);
-	var attributeList=[];
-	var dataEmail={
-	Name:'email',
-	Value:email,
-};
-	var attributeEmail = new AmazonCognitoIdentity.CognitoUserAttribute(dataEmail);
-	attributeList.push(attributeEmail)
-	userPool.signUp(username,password,attributeList,null, function(err,result){
-	if (err){
-	alert(err.message || JSON.stringify(err));
-	return;
-}
-	var cognitoUser=result.user;
-	console.log('Register Successful. User name is :'+cognitoUser.getUsername());
-	//change response
-	document.getElementById("regUserName").value = "";
-	document.getElementById("regPassword").value= "";
-	document.getElementById("regEmail").value= "";
-	document.getElementById("signIn").click()
-	document.getElementById("response").style.visibility="visible"
-	document.getElementById("response").innerText = "Sign Up Successfully. Please Verify Account in Email"
-});}
-	else{
-	alert("Invalid Format of User Input")
+var loginform = document.getElementById('loginform');
+if (loginform.attachEvent) {
+    loginform.attachEvent("submit", processLogin);
+} else {
+    loginform.addEventListener("submit", processLogin);
 }
 
+var registerform = document.getElementById('registerform');
+if (registerform.attachEvent) {
+    registerform.attachEvent("submit", processRegister);
+} else {
+    registerform.addEventListener("submit", processRegister);
 }
 
-	function login() {
 
-	var authenticationData = {
-	Username : document.getElementById("loginUsername").value,
-	Password : document.getElementById("loginPassword").value,
-};
+function cleanPage() {
+    document.getElementById("loginform").reset();
+    document.getElementById("response").innerText = "";
+}
 
-	var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
 
-	var poolData = {
-	UserPoolId : _config.cognito.userPoolId, // Your user pool id here
-	ClientId : _config.cognito.clientId, // Your client id here
-};
+function register() {
 
-	var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
 
-	var userData = {
-	Username : document.getElementById("loginUsername").value,
-	Pool : userPool,
-};
+    username = document.getElementById("regUserName").value;
+    password = document.getElementById("regPassword").value;
+    email = document.getElementById("regEmail").value;
 
-	var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+    var pattern = /[\da-zA-Z]+/;
+    var digit = /[\d]+/;
+    var letter = /[a-zA-Z]+/;
+    var emailRegex = /[^@\s]+@[^@\s]+\.[^@\s]+/;
+    if (pattern.test(username) && pattern.test(password) && password.length >= 8 && digit.test(password) && letter.test(password) && emailRegex.test(email)) {
 
-	cognitoUser.authenticateUser(authenticationDetails, {
-	onSuccess: function (result) {
-	var accessToken = result.getAccessToken().getJwtToken();
-	console.log(accessToken);
-	window.location.replace("main.html");
+        poolData = {
+            UserPoolId: _config.cognito.userPoolId,//AWS Cognito Userpool ID
+            ClientId: _config.cognito.clientId,
+        };
+        var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+        var attributeList = [];
+        var dataEmail = {
+            Name: 'email',
+            Value: email,
+        };
+        var attributeEmail = new AmazonCognitoIdentity.CognitoUserAttribute(dataEmail);
+        attributeList.push(attributeEmail)
+        userPool.signUp(username, password, attributeList, null, function (err, result) {
+            if (err) {
+                alert(err.message || JSON.stringify(err));
+                return;
+            }
+            var cognitoUser = result.user;
+            console.log('Register Successful. User name is :' + cognitoUser.getUsername());
+            //change response
+            document.getElementById("registerform").reset();
 
-},
+            document.getElementById("signIn").click()
+            document.getElementById("response").style.visibility = "visible"
+            document.getElementById("response").innerText = "Sign Up Successfully. Please Verify Account in Email"
+        });
+    } else {
+        alert("Invalid Format of User Input")
+    }
 
-	onFailure: function(err) {
-	document.getElementById("response").style.visibility="visible"
-	document.getElementById("response").innerText = "Error: "+err.message || JSON.stringify(err);
+}
 
-},
-});
+function login() {
+
+
+    var authenticationData = {
+        Username: document.getElementById("loginUsername").value,
+        Password: document.getElementById("loginPassword").value,
+    };
+
+    var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
+
+    var poolData = {
+        UserPoolId: _config.cognito.userPoolId, // Your user pool id here
+        ClientId: _config.cognito.clientId, // Your client id here
+    };
+
+    var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+
+    var userData = {
+        Username: document.getElementById("loginUsername").value,
+        Pool: userPool,
+    };
+
+    var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+
+    cognitoUser.authenticateUser(authenticationDetails, {
+        onSuccess: function (result) {
+            var accessToken = result.getAccessToken().getJwtToken();
+            console.log(accessToken);
+
+            window.location.replace("index.html");
+
+        },
+
+        onFailure: function (err) {
+            document.getElementById("response").style.visibility = "visible";
+            document.getElementById("response").innerText = "Error: " + err.message || JSON.stringify(err);
+
+        },
+    });
 }
 
 
