89a03757c73c9c73fe418f5c6d3efbf0651ffcf5
jqu <jn679016@dal.ca>
2022-03-30 20:58:07 -0300

    restore cognito userpool

--- website/js/config.js
+++ website/js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_AIjU4SGUT', // e.g. us-east-2_uXboG5pAb
+        userPoolId: 'us-east-1_kSAt6tzQ9', // e.g. us-east-2_uXboG5pAb
         region: 'us-east-1', // e.g. us-east-2
-        clientId: 'g3j14n560ql0hr5in9hthjdh' //
+        clientId: '7d8prbg8nloi0p2ia20vpgkn56' //
 },
 };
